ee482416c26c7cd7410c8c8190859cf0c6e3b554
Phil @headlo <support@headlo.com>
2026-06-26 15:31:35 -0700

    Add useService hook for loading PROP services from headlo-sdk

--- deploy-local.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start cmd /k "cd /d D:\headlo\headlo-repo && echo Building SDK and React... && npm run build:react && cd headlo-client-react && npm install && echo Starting local dev server... && npm run dev"
